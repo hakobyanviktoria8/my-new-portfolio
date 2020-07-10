9ef4c178a6a1103f174cbd66d788457c9349bc8f
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-07-10 16:20:31 +0400

    Updates

---
+++ precache-manifest.448959ede81917af70d99d5107ae7682.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f344c0f67c9f8e9c7c8acc46ab7381",
+    "url": "/my-new-portfolio/index.html"
+  },
+  {
+    "revision": "6950e3a0fdd00dbd644f",
+    "url": "/my-new-portfolio/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "488b71aa82df12df251b",
+    "url": "/my-new-portfolio/static/css/main.b7936905.chunk.css"
+  },
+  {
+    "revision": "6950e3a0fdd00dbd644f",
+    "url": "/my-new-portfolio/static/js/2.e478ab43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-new-portfolio/static/js/2.e478ab43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488b71aa82df12df251b",
+    "url": "/my-new-portfolio/static/js/main.6035bca3.chunk.js"
+  },
+  {
+    "revision": "661c0922ee7c7678028f",
+    "url": "/my-new-portfolio/static/js/runtime-main.11982a93.js"
+  },
+  {
+    "revision": "5b543262c1f805bf34d61a06cbde5a28",
+    "url": "/my-new-portfolio/static/media/001.5b543262.jpg"
+  },
+  {
+    "revision": "2dffd8d8365949d1223508b1dd1ed880",
+    "url": "/my-new-portfolio/static/media/004.2dffd8d8.jpg"
+  },
+  {
+    "revision": "04ee9f7e663684b1e7f5964677e725f8",
+    "url": "/my-new-portfolio/static/media/Cake.04ee9f7e.jpg"
+  },
+  {
+    "revision": "af1605438a6a799329cfb8edaf39466f",
+    "url": "/my-new-portfolio/static/media/CovidMapReact.af160543.jpg"
+  },
+  {
+    "revision": "392545d85e618db6ae58380c528ded6e",
+    "url": "/my-new-portfolio/static/media/Exchange.392545d8.jpg"
+  },
+  {
+    "revision": "28d3ebbf884a7f51b2a994190b101be0",
+    "url": "/my-new-portfolio/static/media/Game1.28d3ebbf.jpg"
+  },
+  {
+    "revision": "9d2aff65b859425f60e290a5efe6d8ed",
+    "url": "/my-new-portfolio/static/media/Library.9d2aff65.jpg"
+  },
+  {
+    "revision": "47b678c233008a861605cb33e1845666",
+    "url": "/my-new-portfolio/static/media/Millionaire.47b678c2.jpg"
+  },
+  {
+    "revision": "953eb5026749159c5e0dccac18f7190c",
+    "url": "/my-new-portfolio/static/media/MovieJS.953eb502.jpg"
+  },
+  {
+    "revision": "56829261f671d13d4c55bdb2867536bb",
+    "url": "/my-new-portfolio/static/media/MovieReact.56829261.jpg"
+  },
+  {
+    "revision": "05a7a9d46c12480c72ca021d9a26c805",
+    "url": "/my-new-portfolio/static/media/Screenshot_1.05a7a9d4.jpg"
+  },
+  {
+    "revision": "e0138e4e7e8c1f9d833a47b39cd81c20",
+    "url": "/my-new-portfolio/static/media/Screenshot_1.e0138e4e.jpg"
+  },
+  {
+    "revision": "21a33412e5390e2618afe6e178f5ef8c",
+    "url": "/my-new-portfolio/static/media/Screenshot_10.21a33412.jpg"
+  },
+  {
+    "revision": "2b928a509ced89afa1e8de7c4727225c",
+    "url": "/my-new-portfolio/static/media/Screenshot_11.2b928a50.jpg"
+  },
+  {
+    "revision": "6f96960df5add367bde8e8a35b23eced",
+    "url": "/my-new-portfolio/static/media/Screenshot_12.6f96960d.jpg"
+  },
+  {
+    "revision": "ee6d2fbbdd4fd44336939f1ff1ad3232",
+    "url": "/my-new-portfolio/static/media/Screenshot_13.ee6d2fbb.jpg"
+  },
+  {
+    "revision": "75bbe67304d95aa0f2a7cf01471d8df2",
+    "url": "/my-new-portfolio/static/media/Screenshot_14.75bbe673.jpg"
+  },
+  {
+    "revision": "685ac5693dc3e72a12a6108cee33c2aa",
+    "url": "/my-new-portfolio/static/media/Screenshot_15.685ac569.jpg"
+  },
+  {
+    "revision": "b85c80a1fc79f3d6aef3dfe5e2e613b1",
+    "url": "/my-new-portfolio/static/media/Screenshot_16.b85c80a1.jpg"
+  },
+  {
+    "revision": "7e30728ebdf025f11ec9e86a32b6393d",
+    "url": "/my-new-portfolio/static/media/Screenshot_17.7e30728e.jpg"
+  },
+  {
+    "revision": "316cc74c7fc749b9c66279d53c84bc50",
+    "url": "/my-new-portfolio/static/media/Screenshot_18.316cc74c.jpg"
+  },
+  {
+    "revision": "b3b188a0bfaef1a8e4c7ceb0f514adf0",
+    "url": "/my-new-portfolio/static/media/Screenshot_19.b3b188a0.jpg"
+  },
+  {
+    "revision": "01cfa1c3d6e406ac31e7f87054728d98",
+    "url": "/my-new-portfolio/static/media/Screenshot_2.01cfa1c3.jpg"
+  },
+  {
+    "revision": "e91c589843c5858e86224069d9245771",
+    "url": "/my-new-portfolio/static/media/Screenshot_2.e91c5898.jpg"
+  },
+  {
+    "revision": "0e8f24513ecc164a72936001ca3a8988",
+    "url": "/my-new-portfolio/static/media/Screenshot_20.0e8f2451.jpg"
+  },
+  {
+    "revision": "1173dcfd23dfb1720e68965c743cd776",
+    "url": "/my-new-portfolio/static/media/Screenshot_3.1173dcfd.jpg"
+  },
+  {
+    "revision": "0448ff1a3d20871fdf42294297d24727",
+    "url": "/my-new-portfolio/static/media/Screenshot_4.0448ff1a.jpg"
+  },
+  {
+    "revision": "cc1a635c406233d7abf49d1a27da0de2",
+    "url": "/my-new-portfolio/static/media/Screenshot_5.cc1a635c.jpg"
+  },
+  {
+    "revision": "665bc1572a1b06018febea43d58d27c0",
+    "url": "/my-new-portfolio/static/media/Screenshot_6.665bc157.jpg"
+  },
+  {
+    "revision": "87feccc56fbf452af0a195bb5d845e23",
+    "url": "/my-new-portfolio/static/media/Screenshot_6.87feccc5.png"
+  },
+  {
+    "revision": "40b7233db23be12b8b221d10038595bf",
+    "url": "/my-new-portfolio/static/media/Screenshot_7.40b7233d.jpg"
+  },
+  {
+    "revision": "fd3b61b4438d2c8813fbeb363b3ecffa",
+    "url": "/my-new-portfolio/static/media/Screenshot_8.fd3b61b4.jpg"
+  },
+  {
+    "revision": "63d781439c1b82e8a3dfb1552c8e3bee",
+    "url": "/my-new-portfolio/static/media/Screenshot_9.63d78143.jpg"
+  },
+  {
+    "revision": "1e4f774688aaf0a35a0eff73bb903b82",
+    "url": "/my-new-portfolio/static/media/Time.1e4f7746.jpg"
+  },
+  {
+    "revision": "eb78f691a75da3be698e22060b41ef02",
+    "url": "/my-new-portfolio/static/media/WeatherMap.eb78f691.jpg"
+  },
+  {
+    "revision": "6c9842643a3dd249b2b2d3092ecd6683",
+    "url": "/my-new-portfolio/static/media/WeatherMapFetch.6c984264.jpg"
+  },
+  {
+    "revision": "7b8b8ab09ceb70a0cdbe1e1a6f6cafb0",
+    "url": "/my-new-portfolio/static/media/bootstrat1.7b8b8ab0.png"
+  },
+  {
+    "revision": "e3e821485d7339c7646ea764e8b79e5b",
+    "url": "/my-new-portfolio/static/media/imgCV2.e3e82148.jpg"
+  },
+  {
+    "revision": "d9869b2c018e89018d76ec6332990f2f",
+    "url": "/my-new-portfolio/static/media/react.d9869b2c.png"
+  },
+  {
+    "revision": "d308ff89eb53fa1fd75614fb534c7c3e",
+    "url": "/my-new-portfolio/static/media/restoran.d308ff89.jpg"
+  }
+]);
