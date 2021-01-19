b93589b241f3ca848d85f74ee87e54a26ec25876
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-01-19 14:49:33 +0400

    see the project btn

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -13,11 +13,11 @@ export function Education(props) {
                 </Col>
             </Row>
             <Row>
-                <Col xs="12" sm="10" md="7" lg="7" xl="7">
+                <Col xs="12" sm="10" md="8" lg="8" xl="8">
                     <h3>Armenian Code Academy</h3>
                     <p>12.18.2019 - 30.06.2020</p>
                 </Col>
-                <Col xs="12" sm="10" md="5" lg="5" xl="5">
+                <Col xs="12" sm="10" md="4" lg="4" xl="4">
                     {/*<img src={iconV} alt=""/>*/}
                     <h3>Higher Certificate</h3>
                     <p>JS, React Developer</p>
@@ -25,11 +25,11 @@ export function Education(props) {
             </Row>
             <hr/>
             <Row>
-                <Col xs="12" sm="10" md="7" lg="7" xl="7">
+                <Col xs="12" sm="10" md="8" lg="8" xl="8">
                     <h3>BeeOnCode</h3>
                     <p>01.02.2019 - 30.07.2019</p>
                 </Col>
-                <Col xs="12" sm="10" md="5" lg="5" xl="5">
+                <Col xs="12" sm="10" md="4" lg="4" xl="4">
                     {/*<img src={iconV} alt=""/>*/}
                     <h3>Higher Certificate</h3>
                     <p>Full-Stack Developer</p>
@@ -37,11 +37,11 @@ export function Education(props) {
             </Row>
             <hr/>
             <Row>
-                <Col xs="12" sm="10" md="7" lg="7" xl="7">
+                <Col xs="12" sm="10" md="8" lg="8" xl="8">
                     <h3>Arkofarm</h3>
                     <p>01.02.2012 - 30.06.2012</p>
                 </Col>
-                <Col xs="12" sm="10" md="5" lg="5" xl="5">
+                <Col xs="12" sm="10" md="4" lg="4" xl="4">
                     {/*<img src={iconV} alt=""/>*/}
                     <h3>Higher Certificate</h3>
                     <p>Pharmacy, Manager</p>
@@ -49,11 +49,11 @@ export function Education(props) {
             </Row>
             <hr/>
             <Row>
-                <Col xs="12" sm="10" md="7" lg="7" xl="7">
+                <Col xs="12" sm="10" md="8" lg="8" xl="8">
                     <h3>Armenian State Pedagogical University</h3>
                     <p>01.09.2011 - 30.05.2013</p>
                 </Col>
-                <Col xs="12" sm="10" md="5" lg="5" xl="5">
+                <Col xs="12" sm="10" md="4" lg="4" xl="4">
                     {/*<img src={iconV} alt=""/>*/}
                     <h3>National Diploma</h3>
                     <p>Chemistry teacher</p>
@@ -61,11 +61,11 @@ export function Education(props) {
             </Row>
             <hr/>
             <Row>
-                <Col xs="12" sm="10" md="7" lg="7" xl="7">
+                <Col xs="12" sm="10" md="8" lg="8" xl="8">
                     <h3>National Polytechnic University of Armenia</h3>
                     <p>01.09.2007 - 30.05.2011</p>
                 </Col>
-                <Col xs="12" sm="10" md="5" lg="5" xl="5">
+                <Col xs="12" sm="10" md="4" lg="4" xl="4">
                     {/*<img src={iconV} alt=""/>*/}
                     <h3>National Diploma</h3>
                     <p>Chemist technologist</p>
